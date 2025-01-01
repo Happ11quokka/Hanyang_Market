27c0abf767e8046b3fccaa7b324d98186e01849f
Donghyun <limdongxian1207@gmail.com>
2025-01-01 23:28:13 +0900

    firebase replace

--- cloning/src/firebase.js
+++ cloning/src/firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage"; // Firebase Storage 추가
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBGwRwys-N9v0sh2ycxlzviYWLawRGYKSQ",
-  authDomain: "carrot-c112d.firebaseapp.com",
-  projectId: "carrot-c112d",
-  storageBucket: "carrot-c112d.appspot.com", // storageBucket URL 수정
-  messagingSenderId: "180814873242",
-  appId: "1:180814873242:web:0103e499382f233aa61523",
-  measurementId: "G-RTEGQJ9M3T",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
